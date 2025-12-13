5ee6017de52cc8c5c90e59ffc480ddea058b4610
Razaan <razaanreza0705@gmail.com>
2025-12-14 04:16:30 +0600

    updated routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -6,12 +6,27 @@ import AuthLayout from '../Layout/AuthLayout'
 import PrivateRoute from './PrivateRoute'
 import Login from '../pages/Auth/Login'
 import Register from '../pages/Auth/Register'
+import BookDetails from '../pages/Books/BookDetails'
+import AllBooks from '../pages/Books/AllBooks'
+import MyOrders from '../pages/Dashboard/User/MyOrders'
+import MyProfile from '../pages/Dashboard/User/MyProfile'
+import Wishlist from '../pages/Dashboard/User/Wishlist'
+import Invoices from '../pages/Dashboard/User/Invoices'
+import AddBook from '../pages/Dashboard/Librarian/AddBook'
+import MyBooks from '../pages/Dashboard/Librarian/MyBooks'
+import ManageUsers from '../pages/Dashboard/Admin/ManageUsers'
+import ManageBooks from '../pages/Dashboard/Admin/ManageBooks'
+import Orders from '../pages/Dashboard/Librarian/Orders'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'books', element: <AllBooks /> },
+      { path: 'book/:id', element: <BookDetails /> },
+    ],
   },
   {
     path: '/auth',
@@ -21,7 +36,6 @@ export const router = createBrowserRouter([
       { path: 'register', element: <Register /> },
     ],
   },
-
   {
     path: 'dashboard',
     element: (
@@ -30,7 +44,83 @@ export const router = createBrowserRouter([
       </PrivateRoute>
     ),
     children: [
-      
+      // User Dashboard
+      {
+        path: 'my-orders',
+        element: (
+          <PrivateRoute>
+            <MyOrders />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'my-profile',
+        element: (
+          <PrivateRoute>
+            <MyProfile />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'wishlist',
+        element: (
+          <PrivateRoute>
+            <Wishlist />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'invoices',
+        element: (
+          <PrivateRoute>
+            <Invoices />
+          </PrivateRoute>
+        ),
+      },
+
+      // Librarian Dashboard
+      {
+        path: 'add-book',
+        element: (
+          <PrivateRoute>
+            <AddBook />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'my-books',
+        element: (
+          <PrivateRoute>
+            <MyBooks />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'orders',
+        element: (
+          <PrivateRoute>
+            <Orders />
+          </PrivateRoute>
+        ),
+      },
+
+      // Admin Dashboard
+      {
+        path: 'manage-users',
+        element: (
+          <PrivateRoute>
+            <ManageUsers />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'manage-books',
+        element: (
+          <PrivateRoute>
+            <ManageBooks />
+          </PrivateRoute>
+        ),
+      },
     ],
   },
 ])
