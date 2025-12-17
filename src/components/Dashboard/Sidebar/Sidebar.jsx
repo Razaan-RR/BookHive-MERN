e0fc67669796732bc30312f55ea3f9a33a4e9f03
Razaan <razaanreza0705@gmail.com>
2025-12-17 15:21:43 +0600

    role based functionality added

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,8 +1,58 @@
+// import { Link } from 'react-router-dom'
+// import logo from '../../../../src/assets/logo.png'
+// import { GrLogout } from 'react-icons/gr'
+// import { FcSettings } from 'react-icons/fc'
+// import useAuth from '../../../hooks/useAuth'
+// import MenuItem from '../Menu/MenuItem'
+// import AdminMenu from '../Menu/AdminMenu'
+// import LibrarianMenu from '../Menu/LibrarianMenu'
+// import UserMenu from '../Menu/UserMenu'
+
+// const Sidebar = () => {
+//   const { logOut } = useAuth()
+
+//   return (
+//     <div className="fixed inset-y-0 left-0 w-64 bg-gray-100 px-2 py-4 flex flex-col justify-between">
+//       <div className="flex justify-center mb-6">
+//         <Link to="/">
+//           <img src={logo} alt="BookCourier Logo" width="100" />
+//         </Link>
+//       </div>
+
+//       <nav className="flex-1">
+//         <UserMenu />
+//         <LibrarianMenu />
+//         <AdminMenu />
+//       </nav>
+
+//       <div>
+//         <hr />
+//         <MenuItem
+//           icon={FcSettings}
+//           label="Profile"
+//           address="/dashboard/my-profile"
+//         />
+
+//         <button
+//           onClick={logOut}
+//           className="flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300 transition"
+//         >
+//           <GrLogout className="w-5 h-5" />
+//           <span className="mx-4 font-medium">Logout</span>
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Sidebar
+
 import { Link } from 'react-router-dom'
 import logo from '../../../../src/assets/logo.png'
 import { GrLogout } from 'react-icons/gr'
 import { FcSettings } from 'react-icons/fc'
 import useAuth from '../../../hooks/useAuth'
+import useRole from '../../../hooks/useRole'
 import MenuItem from '../Menu/MenuItem'
 import AdminMenu from '../Menu/AdminMenu'
 import LibrarianMenu from '../Menu/LibrarianMenu'
@@ -10,6 +60,9 @@ import UserMenu from '../Menu/UserMenu'
 
 const Sidebar = () => {
   const { logOut } = useAuth()
+  const [role, isRoleLoading] = useRole()
+
+  if (isRoleLoading) return null
 
   return (
     <div className="fixed inset-y-0 left-0 w-64 bg-gray-100 px-2 py-4 flex flex-col justify-between">
@@ -20,9 +73,9 @@ const Sidebar = () => {
       </div>
 
       <nav className="flex-1">
-        <UserMenu />
-        <LibrarianMenu />
-        <AdminMenu />
+        {role === 'user' && <UserMenu />}
+        {role === 'librarian' && <LibrarianMenu />}
+        {role === 'admin' && <AdminMenu />}
       </nav>
 
       <div>
---
+++ src/hooks/useRole.js
@@ -0,0 +1,21 @@
+import useAuth from './useAuth'
+import useAxiosSecure from './useAxiosSecure'
+import { useQuery } from '@tanstack/react-query'
+
+const useRole = () => {
+  const { user, loading } = useAuth()
+  const axiosSecure = useAxiosSecure()
+
+  const { data: role, isLoading: isRoleLoading } = useQuery({
+    enabled: !loading && !!user?.email,
+    queryKey: ['role', user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/user/role/${user.email}`)
+      return res.data.role
+    },
+  })
+
+  return [role, isRoleLoading]
+}
+
+export default useRole
