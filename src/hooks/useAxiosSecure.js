6a270032951a385d2e9f9d282643d87ced468f97
Razaan <razaanreza0705@gmail.com>
2025-12-22 12:24:16 +0600

    jwt added

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,11 +1,36 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const axiosSecure = axios.create({
+//     baseURL: 'http://localhost:3000',
+//     // You can add headers or other configurations here
+// });
+// const useAxiosSecure = () => {
+//     return axiosSecure;
+// };
+
+// export default useAxiosSecure;
+import axios from 'axios'
+import { getAuth } from 'firebase/auth'
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000',
-    // You can add headers or other configurations here
-});
-const useAxiosSecure = () => {
-    return axiosSecure;
-};
+  baseURL: 'http://localhost:3000',
+})
 
-export default useAxiosSecure;
+// Add a request interceptor to include Firebase token
+axiosSecure.interceptors.request.use(async (config) => {
+  const auth = getAuth()
+  const user = auth.currentUser
+
+  if (user) {
+    const token = await user.getIdToken()
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+const useAxiosSecure = () => {
+  return axiosSecure
+}
+
+export default useAxiosSecure
